8a62ce0fa8918dea0e19bfedf6855fb4d1209e4a
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2024-04-12 17:03:30 -0300

    Add componente ListEmpty e Button

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -4,6 +4,8 @@ import { Highlight } from '@components/highlight';
 import { GroupCard } from '@components/groupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/listEmpty';
+import { Button } from '@components/button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
@@ -25,8 +27,18 @@ export function Groups() {
             title={item} 
           />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+          message="Que tal cadastrar a primeira turma?" 
+          />
+        )}
       />
 
+
+      <Button 
+        title='Criar nova turma'
+      />
     </Container>
   );
 }
