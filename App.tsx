34b00f70949b12aa91ca277b36b255e24abf370a
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2024-04-12 11:48:57 -0300

    Add phosphor-react-native e react-native-svg

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import { Groups } from './src/screens/groups';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { ActivityIndicator } from 'react-native';
+import { Loading } from '@components/loading';
+import { StatusBar } from 'react-native';
 
 
 export default function App() {
@@ -10,7 +11,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups/> : <ActivityIndicator /> }
+     <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <Groups/> : <Loading/> }
     </ThemeProvider>
   );
 }
