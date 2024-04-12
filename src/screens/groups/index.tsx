f1a33c93c2a420e09ac12070719fb9c3e06624bd
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2024-04-12 12:47:49 -0300

    Adicionar  lista de grupos na tela de grupos

--- src/screens/groups/index.tsx
+++ src/screens/groups/index.tsx
@@ -1,10 +1,32 @@
 import { Header } from '@components/header';
 import { Container } from './styles';
+import { Highlight } from '@components/highlight';
+import { GroupCard } from '@components/groupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+  
   return (
     <Container>
       <Header/>
+      
+      <Highlight 
+      title="Turmas"
+      subtitle="jogue com a sua turma"
+      />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+      />
+
     </Container>
   );
 }
