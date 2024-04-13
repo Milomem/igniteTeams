d6fb1dfecdc852c1f66e7d6c29b87adf77edd42e
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2024-04-13 12:55:34 -0300

    Adicionar tratamento de erro nas telas de grupos e jogadores

--- src/screens/players/index.tsx
+++ src/screens/players/index.tsx
@@ -75,12 +75,13 @@ export function Players() {
       setIsLoading(true);
       const playersByTeam = await playersGetByGroupAndTeam(group, team);
       setPlayers(playersByTeam)
-      setIsLoading(false);
 
     } catch (error) {
       console.log(error);
       Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.');
-    }
+    } finally {
+      setIsLoading(false);
+    } 
   }
 
   async function handlePlayerRemove(playerName: string) {
